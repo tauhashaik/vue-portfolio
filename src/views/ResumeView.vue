d6bb40714060fa7f3379f28fd1d52a3387379b0f
tauhashaik <hoosains19@gmail.com>
2024-01-11 09:47:26 +0200

    projects setup

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,13 +1,96 @@
 <template lang="">
-    <div>
+    <body id="Body">
         
-    </div>
+    <div id="WORK">
+        <h6>
+            <ul id="Labels">
+                <li id="Lett">W</li>
+                <li id="Lett">O</li>
+                <li id="Lett">R</li>
+                <li id="Lett">K</li>
+                <li id="Lett"></li>
+                <br>
+                <li id="Lett">XP</li>
+            </ul>
+        </h6>
+     </div>
+
+    </body>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
+<style>
+
+#Labels {
+    list-style: none;
+    color: black;
+    -webkit-text-stroke: 1px grey;
+    /* text-stroke: 1px grey; */
+    font-size: 50px;
+    font-weight: bold;
+    letter-spacing: 10px;
+    font-family: "Audiowide";
+  }
+  
+  #Labels #Lett {
+    display: inline-block;
+    transition: transform 2s, opacity 2s, filter 2s;
+  }
+  
+  #Labels:hover #Lett {
+    transform: rotate(45deg) translateY(-200px);
+    opacity: 0;
+    filter: blur(20px);
+  }
+  
+  #Labels #Lett:nth-child(1) {
+    transition-delay: 0;
+  }
+  
+  #Labels #Lett:nth-child(2) {
+    transition-delay: 0.4s;
+  }
+  
+  #Labels #Lett:nth-child(3) {
+    transition-delay: 0.8s;
+  }
+  
+  #Labels #Lett:nth-child(4) {
+    transition-delay: 1.2s;
+  }
+  
+  #Labels #Lett:nth-child(5) {
+    transition-delay: 1.6s;
+  }
+  #Labels #Lett:nth-child(6) {
+    transition-delay: 2.0s;
+  }
+  #Labels #Lett:nth-child(7) {
+    transition-delay: 2.4s;
+  }
+  #Labels #Lett:nth-child(8) {
+    transition-delay: 3.0s;
+  }
+  #Labels #Lett:nth-child(9) {
+    transition-delay: 3.4s;
+  }
+  #Labels #Lett:nth-child(10) {
+    transition-delay: 3.8s;
+  }
+  #Labels #Lett:nth-child(11) {
+    transition-delay: 4.2s;
+  }
+  #Labels{
+    text-align: center;
+  }
+
+#Body{
+  background-image: url(https://i.postimg.cc/05XpMSwJ/6004798.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 85vh;
+}  
 </style>
